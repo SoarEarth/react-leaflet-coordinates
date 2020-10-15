8715932c7b1e85ab7eb7bda4fdcb530cf645cb0b
Dhruv <dhruvjobanputra8@gmail.com>
2020-10-15 09:54:37 +0800

    Removed decimal/degree/mgrs buttons

--- example/src/App.js
+++ example/src/App.js
@@ -5,39 +5,25 @@ import { Map, TileLayer } from 'react-leaflet'
 import { CoordinatesControl } from 'react-leaflet-box-zoom'
 
 export default class App extends Component {
-	constructor(props) {
-	super(props);
-		this.state = {
-		coord: "decimal"
-		};
-	}
-	handleChange(props) {
-		this.setState({'coord': props})
-	}
+	
 	render () {
 
 		return (
 			<div>
 				<div className="map">
-					<button style={{marginLeft: "10px"}} onClick={() => this.handleChange("decimal")}>Decimal</button>
-					<button onClick={() => this.handleChange("degrees")}>Degrees</button>
-					<button onClick={() => this.handleChange("mgrs")}>MGRS</button>
-					
 					<Map
 						center={[44.635, 22.653]}
 						zoom={12}
 						zoomControl={false} >
 					
-						
-
 						<TileLayer
 							attribution=""
 							url="https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"/>
 
 				
 						<CoordinatesControl 
-							coordinates={this.state.coord}
-							position="topleft"
+							coordinates="decimal"
+							position="bottomleft"
 						/>
 
 					</Map>
